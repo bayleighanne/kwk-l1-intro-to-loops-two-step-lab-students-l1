b583dd6e4c1c84d8998e7580ff247c05eafc02b0
bayleighanne <bayhaf718@gmail.com>
2018-06-06 22:03:28 +0000

    automatically backed up by learn

---
+++ hotelwelcome.rb
@@ -0,0 +1,8 @@
+def tv_welcome(name, room_number)
+  puts "Welcome #{name.upcase}"
+  puts "to the springhill suites" 
+  puts "room #{room_number}"
+  
+end
+
+tv_welcome("Bayleigh", "221")
---
+++ loops.rb
@@ -0,0 +1,33 @@
+# def dance 
+#   moves_slay=0
+  
+#   loop do 
+#     if moves_slay==10
+#       break
+#     end
+#     puts "step to the left"
+#     puts "step to the right"
+#     puts "kick"
+#     moves_slay=moves_slay+1
+# end
+# end 
+# dance
+
+def kwk
+  days = 2 
+  days_left=7
+  #MAKE A WHILE LOOP
+    while days < 10
+    puts "keep coming to camp"
+    #days=days+1
+    puts "you have #{days_left}days left"
+    days += 1
+    days_left-=1
+    end
+puts "camp is over"
+end 
+kwk
+#A WHILE LOOP WORKS WHILE IT IS FALSE 
+#AN UNTIL LOOP WORKS UNTIL IT IS TRUE 
+kwk
+  
